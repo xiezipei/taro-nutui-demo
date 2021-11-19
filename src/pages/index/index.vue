8ea1ec0057bd0d829569bd091e39f5ca2d6d9ce6
xiezipei <zipeixie@gmail.com>
2021-11-19 15:54:55 +0800

    feat: 添加taro组件测试

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,13 +1,16 @@
 <template>
   <view class="index">
     <view>
-      <img src="" alt="">
+      <img src alt />
     </view>
     {{ msg }}
     <view class="btn">
       <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
     </view>
     <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover" />
+    <picker mode="time">
+      <view class="picker">当前选择：{{ timeSel }}</view>
+    </picker>
   </view>
 </template>
 
@@ -16,15 +19,19 @@ import { reactive, toRefs } from 'vue';
 export default {
   name: 'Index',
   components: {
-    
+
   },
-  setup(){
+  setup() {
     const state = reactive({
       msg: '欢迎使用 NutUI3.0 开发小程序',
       msg2: '你成功了～',
       type: 'text',
       show: false,
-      cover: false
+      cover: false,
+      selector: ['美国', '中国', '巴西', '日本'],
+      selectorChecked: '美国',
+      timeSel: '12:01',
+      dateSel: '2018-04-22'
     });
 
     const handleClick = (type, msg, cover = false) => {
